9295ed583e0a95968638e0ed51a1185f532156c3
Luigi Breda <luigi.breda@meuk8.com.br>
2024-03-06 04:18:08 -0300

    imagem kaka

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,9 +3,6 @@ import { LoginButton } from '@/components/login-button'
 import { redirect } from 'next/navigation'
 import { IconGitHub, IconGoogle } from '@/components/ui/icons'; 
 
-// Adicione seus próprios caminhos de importação conforme necessário
-import AssistenteVirtualImg from '@/assets/assistente-virtual-kaka.jpg'; // Caminho para a imagem do assistente
-
 export default async function SignInPage() {
   const session = await auth()
   // redirect to home if user is already logged in
@@ -18,7 +15,7 @@ export default async function SignInPage() {
       <div className="flex flex-col space-y-4"> {/* Ajuste o layout conforme necessário */}
         {/* Card do Assistente Virtual */}
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-          <img className="w-full" src={AssistenteVirtualImg} alt="Kaká - Assistente Virtual da K8" />
+          <img className="w-full" src='kaka-k8-ia.jpg' alt="Kaká - Assistente Virtual da K8" />
           <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">Kaká - Assistente Virtual da K8</div>
             <p className="text-gray-700 text-base">
