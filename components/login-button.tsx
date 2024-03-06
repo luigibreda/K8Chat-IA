67f2c9b45d65bf467eb6029f19bd23c222ea05de
Luigi Breda <luigi.breda@meuk8.com.br>
2024-03-06 01:59:53 -0300

    Teste provedor Google

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -5,27 +5,36 @@ import { signIn } from 'next-auth/react'
 
 import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
-import { IconGitHub, IconSpinner } from '@/components/ui/icons'
+import { IconGitHub, IconSpinner, IconDownload } from '@/components/ui/icons'
 
 interface LoginButtonProps extends ButtonProps {
-  showGithubIcon?: boolean
+  provider?: string
+  showIcon?: boolean
   text?: string
 }
 
 export function LoginButton({
+  provider = 'github', // Adiciona uma prop 'provider' com 'github' como default
   text = 'Login with GitHub',
-  showGithubIcon = true,
+  showIcon  = true,
   className,
   ...props
 }: LoginButtonProps) {
   const [isLoading, setIsLoading] = React.useState(false)
+
+  // Ajusta o texto e ícone dependendo do provedor
+  let ProviderIcon = showIcon ? IconGitHub : null;
+  if (provider === 'google') {
+    text = 'Login with Google';
+    ProviderIcon = showIcon ? IconDownload : null; // Certifique-se de ter um componente IconGoogle
+  }
+
   return (
     <Button
       variant="outline"
       onClick={() => {
-        setIsLoading(true)
-        // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-        signIn('github', { callbackUrl: `/` })
+        setIsLoading(true);
+        signIn(provider, { callbackUrl: `/` });
       }}
       disabled={isLoading}
       className={cn(className)}
@@ -33,10 +42,31 @@ export function LoginButton({
     >
       {isLoading ? (
         <IconSpinner className="mr-2 animate-spin" />
-      ) : showGithubIcon ? (
-        <IconGitHub className="mr-2" />
+      ) : ProviderIcon ? (
+        <ProviderIcon className="mr-2" />
       ) : null}
       {text}
     </Button>
-  )
+  );
+
+  // return (
+  //   <Button
+  //     variant="outline"
+  //     onClick={() => {
+  //       setIsLoading(true)
+  //       // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
+  //       signIn('github', { callbackUrl: `/` })
+  //     }}
+  //     disabled={isLoading}
+  //     className={cn(className)}
+  //     {...props}
+  //   >
+  //     {isLoading ? (
+  //       <IconSpinner className="mr-2 animate-spin" />
+  //     ) : showIcon  ? (
+  //       <IconGitHub className="mr-2" />
+  //     ) : null}
+  //     {text}
+  //   </Button>
+  // )
 }
